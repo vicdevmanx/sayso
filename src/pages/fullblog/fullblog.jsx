16514b194bf3ef596aafb0f4dadad55b4597f039
unknown <you@example.com>
2025-05-22 22:00:09 +0100

    tried again

--- src/pages/fullblog/fullblog.jsx
+++ src/pages/fullblog/fullblog.jsx
@@ -34,6 +34,7 @@ const Fullblog = ({ username, profilepic, readtime, date, title, tags = ['nice',
 
     useEffect(() => {
         fetchPost()
+        console.log('called fetch')
     })
 
     return (
@@ -43,7 +44,7 @@ const Fullblog = ({ username, profilepic, readtime, date, title, tags = ['nice',
                     <div className='flex gap-2 items-center'>
                         <ChevronLeft onClick={() => navigate(-1)} className='size-10 -ml-2 rounded-full transition p-2 hover:bg-[#272b34] active:bg-[#272b34]' /><div className='bg-[#272b34] w-12 h-12 rounded-full flex justify-center items-center cursor-pointer transition hover:bg-[#1c1f26]'>{<img src={data?.users?.image_url} className='object-fit w-full h-auto' /> && <User size={18} />}</div>
                         <div className=" text-[#bbbbcc] font-[poppins-medium] flex flex-col -gap-1.5">
-                            <p className="text-white text-[13px]">{data?.users.username || 'anonymous'}</p>
+                            <p className="text-white text-[13px]">{data?.users?.username || 'anonymous'}</p>
                             <p className='flex items-center gap-1.5 text-xs'>{date || 'today'}<span className='bg-[#bbbbcc] w-1 h-1 rounded-full'></span> {readtime || '20 min'} read </p>
                         </div>
                     </div>
