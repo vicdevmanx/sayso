8b51f9f335f2c7df5d7e147605b0ea9eb228c0a2
unknown <you@example.com>
2025-05-22 22:15:54 +0100

    fixed useEffect

--- src/pages/fullblog/fullblog.jsx
+++ src/pages/fullblog/fullblog.jsx
@@ -7,6 +7,7 @@ import { useMediaQuery } from "@mui/material";
 import clsx from 'clsx'
 import { useNavigate } from 'react-router-dom'
 import { useParams } from "react-router-dom";
+import Loader from "@/assets/loader/loader";
 
 const Fullblog = ({ username, profilepic, readtime, date, title, tags = ['nice', 'good'], postImg, likes, comment }) => {
     const [liked, setLiked] = useState(false)
@@ -34,17 +35,16 @@ const Fullblog = ({ username, profilepic, readtime, date, title, tags = ['nice',
 
     useEffect(() => {
         fetchPost()
-        console.log('called fetch')
-    })
+    },[])
 
     return (
         <div className='bg-[#0e1116] font-[poppins] text-[#f5f5f5] w-full min-h-screen h-full relative'>
             <div className="min-h-screen flex flex-col m-auto p-2 py-4 w-full max-w-2xl gap-4">
                 <div className='flex justify-between w-full items-center'>
                     <div className='flex gap-2 items-center'>
-                        <ChevronLeft onClick={() => navigate(-1)} className='size-10 -ml-2 rounded-full transition p-2 hover:bg-[#272b34] active:bg-[#272b34]' /><div className='bg-[#272b34] w-12 h-12 rounded-full flex justify-center items-center cursor-pointer transition hover:bg-[#1c1f26]'>{<img src={data?.users?.image_url} className='object-fit w-full h-auto' /> && <User size={18} />}</div>
+                        <ChevronLeft onClick={() => navigate(-1)} className='size-10 -ml-2 rounded-full transition p-2 hover:bg-[#272b34] active:bg-[#272b34]' /><div className='bg-[#272b34] w-12 h-12 rounded-full flex justify-center items-center cursor-pointer transition hover:bg-[#1c1f26]'>{<img src={data?.users?.image_url} className='object-fit w-full h-auto' /> || <User size={18} />}</div>
                         <div className=" text-[#bbbbcc] font-[poppins-medium] flex flex-col -gap-1.5">
-                            <p className="text-white text-[13px]">{data?.users?.username || 'anonymous'}</p>
+                            <p className="text-white text-[13px]">{data?.users?.username || 'Loading...'}</p>
                             <p className='flex items-center gap-1.5 text-xs'>{date || 'today'}<span className='bg-[#bbbbcc] w-1 h-1 rounded-full'></span> {readtime || '20 min'} read </p>
                         </div>
                     </div>
@@ -55,20 +55,20 @@ const Fullblog = ({ username, profilepic, readtime, date, title, tags = ['nice',
                         />
                     </div>
                 </div>
-                <h1 className="text-2xl font-[poppins-bold] text-white mb-4">{data?.title}</h1>
+                <h1 className="text-2xl font-[poppins-bold] text-white mb-4">{data?.title || 'loading...'}</h1>
                 <div className='flex gap-2 flex-wrap items-center'>
                     {data?.tags && data?.tags.map((tag) => (
                         <p className='text-[10.5px] font-[poppins-medium] border-2 border-[#272b34] text-[#717889] px-1.5 py-1 rounded-lg cursor-pointer select-none cursor-pointer'>{tag}</p>
                     ))}
                 </div>
-                <p className='text-sm'>{data?.content} || test content</p>
+                <p className='text-sm'>{data?.content || <Loader size={10}/>}</p>
 
                 <div className="bg-[#1c1f26] shadow-lg rounded-lg w-full min-h-32 h-auto">
-                    {/* <img
-                        src={data.image_url}
+                    <img
+                        src={data?.image_url}
                         alt="Blog Avatar"
                         className="w-full"
-                    /> */}
+                    />
                 </div>
 
                 <div className='w-full b-0 h-full flex relative items-center gap-4 bg-[#1c1f26] p-2 rounded-lg'>
